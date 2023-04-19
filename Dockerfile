b5458d74392cf0896b0a29a9b01285e7e7c69dd3
Mike_Sar <saragasmixalis@gmail.com>
2023-04-19 16:28:42 +0300

    corrected compose file

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM python:3.8
+FROM python:3.9
 
 WORKDIR /app
 
 COPY app/ .
 # Install Elasticsearch-Py library (pinned to version 7.0.5)
-RUN pip3 install elasticsearch==7.0.5
+RUN pip3 install opensearch-py
+
+EXPOSE 9200
 
 CMD ["python3", "main.py"]
--- app/main.py
+++ app/main.py
@@ -1,46 +1,23 @@
-from elasticsearch import Elasticsearch
-import base64
+from opensearchpy import OpenSearch
 
-# Create an instance of the Elasticsearch client
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+# Create an OpenSearch client instance
+client = OpenSearch(
+    hosts=[{'host': 'localhost', 'port': 9200}],
+    #http_auth=('admin', 'admin')
+)
 
-# Define an index name
-index_name = 'my_index'
-
-# Create an index with mappings
-es.indices.create(index=index_name, ignore=400, body={
-    'mappings': {
-        'properties': {
-            'name': {'type': 'text'},
-            'age': {'type': 'integer'}
-        }
-    }
-})
-
-# Index a document
-document = {
-    'name': 'Mike Saragas',
-    'age': 27
-}
-es.index(index=index_name, doc_type='_doc', document=document)
-
-# Search for documents
+# Define an index and a search query
+index = 'my_index'
 query = {
-    'query': {
-        'match': {
-            'name': 'Mike'
-        }
+    "query": {
+        "match_all": {}
     }
 }
-result = es.search(index=index_name, body=query)
+
+# Perform a search operation
+response = client.search(index=index, body=query)
 
 # Print search results
 print("Search results:")
-for hit in result['hits']['hits']:
-    print(f"Document ID: {hit['_id']}")
-    print(f"Name: {hit['_source']['name']}")
-    print(f"Age: {hit['_source']['age']}")
-    print('---')
-
-# Delete the index
-es.indices.delete(index=index_name, ignore=[400, 404])
+for hit in response['hits']['hits']:
+    print(hit['_source'])
